9dbc5f7a4dadab1994b268f01ebf29ebefaf3830
sushil kumar <sushil1thakur@gmail.com>
2022-12-20 12:17:31 +0530

    commit

---
+++ src/main/java/com/example/springgithub/SpringGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springgithub/SpringGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
