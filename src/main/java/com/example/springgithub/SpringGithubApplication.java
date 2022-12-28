3f6d3d03b3e5c1f5748e85b8f3abb360d8a70289
sushil kumar <sushil1thakur@gmail.com>
2022-12-28 12:08:29 +0530

    change in pom.xm and springbootApplication

--- src/main/java/com/example/springgithub/SpringGithubApplication.java
+++ src/main/java/com/example/springgithub/SpringGithubApplication.java
@@ -2,10 +2,19 @@ package com.example.springgithub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringGithubApplication {
+public class SpringGithubApplication extends SpringBootServletInitializer {
 
+	
+	@Override  
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)   
+	{  
+	return application.sources(SpringGithubApplication.class);  
+	}  
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGithubApplication.class, args);
 	}
